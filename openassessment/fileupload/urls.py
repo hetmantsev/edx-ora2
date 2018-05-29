114b2eb3473395c5541d058e0229b0908bbd4ea0
Danylo Hetmantsev <hetmantsev@gmail.com>
2018-05-29 20:47:59 +0300

    Update urls.py

--- openassessment/fileupload/urls.py
+++ openassessment/fileupload/urls.py
@@ -5,4 +5,5 @@ from openassessment.fileupload import views_django_storage, views_filesystem
 urlpatterns = [
     url(r'^django/(?P<key>.+)/$', views_django_storage.django_storage, name='openassessment-django-storage'),
     url(r'^(?P<key>.+)/$', views_filesystem.filesystem_storage, name='openassessment-filesystem-storage'),
+    url(r'^openassessment/storage', include(openassessment.fileupload.urls)),
 ]
